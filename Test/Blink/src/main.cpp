761638b318771a964a46bc09e3b15b65204213e7
Fauzi Abdul Rohim <fauziboyranger@gmail.com>
2023-05-31 22:05:08 +0700

    Update main.cpp

--- Test/Blink/src/main.cpp
+++ Test/Blink/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+// int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // int result = myFunction(2, 3);
+  pinMode(D4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(D4, HIGH);
+  delay(500);
+   digitalWrite(D4, LOW);
+  delay(500);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
