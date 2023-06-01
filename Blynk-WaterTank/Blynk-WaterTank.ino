4f75604c90d40fb26ebb3a0fd55b07603f14a7d3
Fauzi Abdul Rohim <fauziboyranger@gmail.com>
2023-06-01 11:14:13 +0700

    Create Blynk-WaterTank.ino

---
+++ Blynk-WaterTank/Blynk-WaterTank.ino
@@ -0,0 +1,54 @@
+// ---------------------------------------------------------------------------
+// Example NewPing library sketch that does a ping about 20 times per second.
+// ---------------------------------------------------------------------------
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+/* Fill in information from Blynk Device Info here */
+//#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
+//#define BLYNK_TEMPLATE_NAME         "Device"
+//#define BLYNK_AUTH_TOKEN            "YourAuthToken"
+
+#define BLYNK_TEMPLATE_ID "TMPL6iJn0iMil"
+#define BLYNK_TEMPLATE_NAME "SmartWaterTan"
+#define BLYNK_AUTH_TOKEN "IKDattlGNHRgDgnMbM-mRJIJzeQV6X8u"
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <NewPing.h>
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Lab";
+char pass[] = "12345678";
+
+
+
+#define TRIGGER_PIN D0    // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN D1       // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200  // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);  // NewPing setup of pins and maximum distance.
+
+BlynkTimer timer;
+void sendSensor() {
+  delay(50);  // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm());  // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
+  Blynk.virtualWrite(V0, sonar.ping_cm());
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run();
+}
