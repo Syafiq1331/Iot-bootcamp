3b925feaf4883d9b95a045c7ee412924defb99fe
Fauzi Abdul Rohim <fauziboyranger@gmail.com>
2023-06-03 11:02:56 +0700

    add smart farm

---
+++ smartFarm/src/main.cpp
@@ -0,0 +1,16 @@
+// PROJECT 5: SMART FARM USING UBIDOTS
+// DHT11, HCSR-04, MQ-135, LED, Motor Servo dan Relay
+
+
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
